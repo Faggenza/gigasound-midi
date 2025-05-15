1028bb92207b7a9662202a82d336c5837025055b
Nicola Guerrera <guerrera.nicola@gmail.com>
2025-05-15 16:20:56 +0200

    added tinyusb example midi

--- include/init.h
+++ include/init.h
@@ -3,6 +3,7 @@ void MX_GPIO_Init(void);
 void MX_ADC1_Init(void);
 void MX_SPI3_Init(void);
 void MX_DMA_Init(void);
+void MX_USB_OTG_FS_PCD_Init(void);
 extern ADC_HandleTypeDef hadc1;
 extern SPI_HandleTypeDef hspi3;
 extern DMA_HandleTypeDef hdma_spi3_tx;
