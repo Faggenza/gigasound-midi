43fa25ce7e0d77a4bf7633203eb933b913fdb40b
Faggenza <ik3cco@gmail.com>
2025-06-18 12:48:28 +0200

    fw: rotate screen

--- include/ssd1306.h
+++ include/ssd1306.h
@@ -22,21 +22,39 @@ void SSD1306_MINIMAL_init()
 {
   unsigned char initialization[] = {
       /* Enable charge pump regulator (RESET = ) */
-      0x80, 0x8d,
-      0x80, 0x14,
+      0x80,
+      0x8d,
+      0x80,
+      0x14,
       /* Display On (RESET = ) */
-      0x80, 0xaf,
+      0x80,
+      0xaf,
       /* Set Memory Addressing Mode to Horizontal Addressing Mode (RESET = Page Addressing Mode) */
-      0x80, 0x20,
-      0x80, 0x0,
+      0x80,
+      0x20,
+      0x80,
+      0x0,
       /* Reset Column Address (for horizontal addressing) */
-      0x80, 0x21,
-      0x80, 0,
-      0x80, 127,
+      0x80,
+      0x21,
+      0x80,
+      0,
+      0x80,
+      127,
       /* Reset Page Address (for horizontal addressing) */
-      0x80, 0x22,
-      0x80, 0,
-      0x80, 7};
+      0x80,
+      0x22,
+      0x80,
+      0,
+      0x80,
+      7,
+      /*Set Left/Right rotation*/
+      0x80,
+      0xa0 | 0x01,
+      /*Set Scan Direction*/
+      0x80,
+      0xc0 | 0x08,
+  };
 
   HAL_I2C_Master_Transmit(&hi2c1, SSD1306_MINIMAL_SLAVE_ADDR, initialization, sizeof(initialization), UINT32_MAX);
 }
